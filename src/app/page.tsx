c55302ceb80a2e9305e5e4ce7cbc82d078c0eb15
Ye Tun Aung <ysochannel2@gmail.com>
2025-06-26 15:16:43 +0000

    Change wheel location from current location to alittle upper side to loo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -89,7 +89,7 @@ export default function Home() {
 
   return (
     <div className="flex min-h-svh w-full flex-col md:flex-row bg-background text-foreground">
-      <main className="w-full md:w-2/3 flex flex-col items-center justify-center p-4 md:p-8">
+      <main className="w-full md:w-2/3 flex flex-col items-center p-4 md:p-8">
         <div className="relative flex items-center justify-center w-full max-w-[400px] md:max-w-[600px] aspect-square">
             <WheelCanvas
               ref={wheelRef}
